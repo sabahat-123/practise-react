04c599cc9879be794eceb3fff185adad87dba3b4
sabahat-123 <sabahat.parveen@imtspl.net>
2023-07-13 18:52:42 +0530

    initial comit

---
+++ practise/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Signup from './Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Signup/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ practise/src/Signup.js
@@ -0,0 +1,93 @@
+import './App.css';
+import {useState} from 'react'
+export default function Signup(){
+    const [data , setData] = useState({
+        fullName : '',
+        email : '',
+        create_password : '',
+        confirm_password:''
+    })
+    const changeHandle = (e) =>{
+       const{name , value} = e.target;
+    //    console.log(name,value);
+       const newValue = data;
+       data[name] = value;
+    //    console.log(data[name]);
+       setData(newValue);
+       
+    }
+    const handleSubmit =(e)=>{
+       e.preventDefault();
+       console.log(data);
+       localStorage.setItem('TotalData' , JSON.stringify(data));
+    }
+    return(
+        <div className="container">
+             <form  className='form' onSubmit={handleSubmit}>
+                <h2>Validation Form</h2>
+                <div className="row">
+                    <div className="col-25">
+                    <label for="fullname">Full Name</label>
+                    </div>
+                    <div className="col-75">
+                    <input
+                    type='text'
+                    placeholder='Enter your name'
+                    name='fullName'
+                    value={data.fullName.value}
+                    onChange={changeHandle}
+                    />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label for="email">Email</label>
+                    </div>
+                    <div className="col-75">
+                        <input
+                        type="email"
+                        placeholder='Enter your email'
+                        name='email'
+                        value={data.email.value}
+                        onChange={changeHandle}
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label for="number">Create Password</label>
+                    </div>
+                    <div className="col-75">
+                        <input
+                        type="password"
+                        placeholder='Enter new password'
+                        name='create_password'
+                        value={data.create_password.value}
+                        onChange={changeHandle}
+                        
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-25">
+                        <label for="password">Confirm Password</label>
+                    </div>
+                    <div className="col-75">
+                        <input
+                        type="password"
+                        placeholder='Enter your password'
+                        name='confirm_password'
+                        value={data.confirm_password.value}
+                        onChange={changeHandle}
+                        
+                        />
+                    </div>
+                </div>
+                <div className="row">
+                <input type="submit" value="Sign up"/>
+                </div>
+            </form>
+
+        </div>
+    )
+}
